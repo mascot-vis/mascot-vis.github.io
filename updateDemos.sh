cf8dd9b53519479c6ef1b311490cd8cb76a9640d
zcliu <zhicheng.liu@gmail.com>
2026-07-21 12:57:46 -0400

    updateDemos script

---
+++ updateDemos.sh
@@ -0,0 +1,11 @@
+cp -f ../Mascot/index.html static/gallery.html
+cp -r -f ../Mascot/demos/*.json static/demos/
+cp -r -f ../Mascot/demos/static/*.js static/demos/static/
+cp -r -f ../Mascot/demos/interactive/*.js static/demos/interactive/
+cp -r -f ../Mascot/demos/thumbnails/*.png static/demos/thumbnails/
+cp -f ../Mascot/datasets/csv/*.csv static/datasets/csv/
+cp -f ../Mascot/datasets/graphjson/*.json static/datasets/graphjson/
+cp -f ../Mascot/datasets/treejson/*.json static/datasets/treejson/
+cp -f ../Mascot/lib/*.js static/lib/
+cp -f ../Mascot/js/*.js static/js/
+cp -f ../Mascot/dist/*.js static/dist/
